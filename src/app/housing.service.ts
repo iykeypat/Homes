cc0f046bc788abfc8fcf36fc95b1a09d631a7e7b
ik ikey <patrickiykey@gmail.com>
2023-06-16 16:04:40 +0100

    refactored housing service

--- src/app/housing.service.ts
+++ src/app/housing.service.ts
@@ -7,108 +7,108 @@ import { HousingLocation } from './housinglocation';
 export class HousingService {
 
   constructor() { }
-  // housingLocationList: HousingLocation[] = [
-  //   {
-  //     id: 0,
-  //     name: 'Acme Fresh Start Housing',
-  //     city: 'Chicago',
-  //     state: 'IL',
-  //     photo: '/assets/photo-1.jpg',
-  //     availableUnits: 4,
-  //     wifi: true,
-  //     laundry: true
-  //   },
-  //   {
-  //     id: 1,
-  //     name: 'A113 Transitional Housing',
-  //     city: 'Santa Monica',
-  //     state: 'CA',
-  //     photo: '/assets/photo-2.jpg',
-  //     availableUnits: 0,
-  //     wifi: false,
-  //     laundry: true
-  //   },
-  //   {
-  //     id: 2,
-  //     name: 'Warm Beds Housing Support',
-  //     city: 'Juneau',
-  //     state: 'AK',
-  //     photo: '/assets/photo-3.jpg',
-  //     availableUnits: 1,
-  //     wifi: false,
-  //     laundry: false
-  //   },
-  //   {
-  //     id: 3,
-  //     name: 'Homesteady Housing',
-  //     city: 'Chicago',
-  //     state: 'IL',
-  //     photo: '/assets/photo-4.jpg',
-  //     availableUnits: 1,
-  //     wifi: true,
-  //     laundry: false
-  //   },
-  //   {
-  //     id: 4,
-  //     name: 'Happy Homes Group',
-  //     city: 'Gary',
-  //     state: 'IN',
-  //     photo: '/assets/photo-5.jpg',
-  //     availableUnits: 1,
-  //     wifi: true,
-  //     laundry: false
-  //   },
-  //   {
-  //     id: 5,
-  //     name: 'Hopeful Apartment Group',
-  //     city: 'Oakland',
-  //     state: 'CA',
-  //     photo: '/assets/photo-6.jpg',
-  //     availableUnits: 2,
-  //     wifi: true,
-  //     laundry: true
-  //   },
-  //   {
-  //     id: 6,
-  //     name: 'Seriously Safe Towns',
-  //     city: 'Oakland',
-  //     state: 'CA',
-  //     photo: '/assets/photo-7.jpg',
-  //     availableUnits: 5,
-  //     wifi: true,
-  //     laundry: true
-  //   },
-  //   {
-  //     id: 7,
-  //     name: 'Hopeful Housing Solutions',
-  //     city: 'Oakland',
-  //     state: 'CA',
-  //     photo: '/assets/photo-8.jpg',
-  //     availableUnits: 2,
-  //     wifi: true,
-  //     laundry: true
-  //   },
-  //   {
-  //     id: 8,
-  //     name: 'Seriously Safe Towns',
-  //     city: 'Oakland',
-  //     state: 'CA',
-  //     photo: '/assets/photo-9.jpg',
-  //     availableUnits: 10,
-  //     wifi: false,
-  //     laundry: false
-  //   },
-  //   {
-  //     id: 9,
-  //     name: 'Capital Safe Towns',
-  //     city: 'Portland',
-  //     state: 'OR',
-  //     photo: '/assets/photo-10.jpg',
-  //     availableUnits: 6,
-  //     wifi: true,
-  //     laundry: true
-  //   }
-  // ];
+  housingLocationList: HousingLocation[] = [
+    {
+      id: 0,
+      name: 'Acme Fresh Start Housing',
+      city: 'Chicago',
+      state: 'IL',
+      photo: '/assets/photo-1.jpg',
+      availableUnits: 4,
+      wifi: true,
+      laundry: true
+    },
+    {
+      id: 1,
+      name: 'A113 Transitional Housing',
+      city: 'Santa Monica',
+      state: 'CA',
+      photo: '/assets/photo-2.jpg',
+      availableUnits: 0,
+      wifi: false,
+      laundry: true
+    },
+    {
+      id: 2,
+      name: 'Warm Beds Housing Support',
+      city: 'Juneau',
+      state: 'AK',
+      photo: '/assets/photo-3.jpg',
+      availableUnits: 1,
+      wifi: false,
+      laundry: false
+    },
+    {
+      id: 3,
+      name: 'Homesteady Housing',
+      city: 'Chicago',
+      state: 'IL',
+      photo: '/assets/photo-4.jpg',
+      availableUnits: 1,
+      wifi: true,
+      laundry: false
+    },
+    {
+      id: 4,
+      name: 'Happy Homes Group',
+      city: 'Gary',
+      state: 'IN',
+      photo: '/assets/photo-5.jpg',
+      availableUnits: 1,
+      wifi: true,
+      laundry: false
+    },
+    {
+      id: 5,
+      name: 'Hopeful Apartment Group',
+      city: 'Oakland',
+      state: 'CA',
+      photo: '/assets/photo-6.jpg',
+      availableUnits: 2,
+      wifi: true,
+      laundry: true
+    },
+    {
+      id: 6,
+      name: 'Seriously Safe Towns',
+      city: 'Oakland',
+      state: 'CA',
+      photo: '/assets/photo-7.jpg',
+      availableUnits: 5,
+      wifi: true,
+      laundry: true
+    },
+    {
+      id: 7,
+      name: 'Hopeful Housing Solutions',
+      city: 'Oakland',
+      state: 'CA',
+      photo: '/assets/photo-8.jpg',
+      availableUnits: 2,
+      wifi: true,
+      laundry: true
+    },
+    {
+      id: 8,
+      name: 'Seriously Safe Towns',
+      city: 'Oakland',
+      state: 'CA',
+      photo: '/assets/photo-9.jpg',
+      availableUnits: 10,
+      wifi: false,
+      laundry: false
+    },
+    {
+      id: 9,
+      name: 'Capital Safe Towns',
+      city: 'Portland',
+      state: 'OR',
+      photo: '/assets/photo-10.jpg',
+      availableUnits: 6,
+      wifi: true,
+      laundry: true
+    }
+  ];
 
   // returns all housing locations
   url = 'http://localhost:3000/locations';
@@ -118,8 +118,8 @@ export class HousingService {
   //   return this.housingLocationList;
   // }
   async getAllHousingLocations(): Promise<HousingLocation[]> {
-    const data = await fetch(this.url);
-    return await data.json() ?? [];
+    const data = this.housingLocationList;//await fetch(this.url);
+    return data;//await data.json() ?? [];
   }
 
 
@@ -128,8 +128,9 @@ export class HousingService {
   //   return this.housingLocationList.find(housingLocation => housingLocation.id === id);
   // }
   async getHousingLocationById(id: number): Promise<HousingLocation | undefined> {
-    const data = await fetch(`${this.url}/${id}`);
-    return await data.json() ?? {};
+    // const data = await fetch(`${this.url}/${id}`);
+    // return await data.json() ?? {};
+    return this.housingLocationList.find(x => x.id === id);
   }
 
   //for form collection
